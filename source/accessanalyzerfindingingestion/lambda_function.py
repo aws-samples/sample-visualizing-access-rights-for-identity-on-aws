e030e2935ebc0c71c2149fcf7a84f5048da9842e
Alex Waddell <waddeam@amazon.co.uk>
2025-06-25 20:36:32 +0100

    Updated AA ingestion function to support deletion of resolved or archived findings

--- source/accessanalyzerfindingingestion/lambda_function.py
+++ source/accessanalyzerfindingingestion/lambda_function.py
@@ -115,6 +115,34 @@ def parse_unusedaccess_finding(event,table_ua):
     # Add the item to the DynamoDB table
     table_ua.put_item(Item=item)
 
+def delete_item_by_finding_id(table_name, finding_id):
+    try:
+        # Create DynamoDB resource
+        dynamodb = boto3.resource('dynamodb')
+        
+        # Get the table
+        table = dynamodb.Table(table_name)
+        
+        # Delete the item
+        response = table.delete_item(
+            Key={
+                'FindingId': finding_id
+            }
+        )
+        
+        # Check if the deletion was successful
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            print(f"Successfully deleted item with FindingId: {finding_id}")
+            return True
+        else:
+            print(f"Failed to delete item with FindingId: {finding_id}")
+            return False
+        
+    except ClientError as e:
+        print(f"Error deleting item: {e}")
+        return False
+
+
 def extract_role_name(arn):
     # Split by '/' and get the last element
     role_name = arn.split('/')[-1]
@@ -124,12 +152,19 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table_ia = dynamodb.Table('AriaIdCInternalAAFindings')
     table_ua = dynamodb.Table('AriaIdCUnusedAAFindings')
+    table_ea = dynamodb.Table('AriaIdCExternalAAFindings')
     
     try:    
         if (event['detail']['findingType'] == 'InternalAccess'):
-            parse_internalaccess_finding(event, table_ia)
+            if (event['detail']['status'] == 'RESOLVED'):
+                delete_item_by_finding_id(event['detail']['findingId'], table_ia))
+            else:
+                parse_internalaccess_finding(event, table_ia)
         elif (event['detail']['findingType'] == 'UnusedPermission' or 'UnusedIAMRole'):
-            parse_unusedaccess_finding(event, table_ua)
+            if (event['detail']['status'] == 'ARCHIVED'):
+                delete_item_by_finding_id(event['detail']['findingId'], table_ua))
+            else:
+                parse_unusedaccess_finding(event, table_ua)
 
         # print(f"Successfully added finding {finding_id} to DynamoDB")
         return {
